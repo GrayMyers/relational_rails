f3cb667b11f90a8bdcf5cfd3a614e6af0e69791f
RyanDBarnett <conner2468@gmail.com>
2020-12-02 00:32:44 -0700

    Add ships index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/ports', to: 'ports#index'
   get '/ports/:id', to: 'ports#show'
+  get '/ships', to: 'ships#index'
   get '/vehicles', to: 'vehicles#index'
   get '/passengers', to: 'passengers#index'
   get 'vehicles/:id', to: 'vehicles#show'
