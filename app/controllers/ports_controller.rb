099f066e54450143602bd086dd7135171b2546c2
RyanDBarnett <conner2468@gmail.com>
2020-12-04 11:43:07 -0700

    Add new and create actions to port controller w port strong params

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -6,4 +6,19 @@ class PortsController < ApplicationController
   def show
     @port = Port.find(params[:id])
   end
+
+  def new
+    @port = Port.new
+  end
+
+  def create
+    Port.create(port_params)
+    redirect_to '/ports'
+  end
+
+  private
+
+  def port_params
+    params.require(:port).permit(:name, :dock_count, :panamax)
+  end
 end
