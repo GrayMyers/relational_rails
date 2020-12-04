17d805ff8b4318908a0b89f4a9304bde51ba92c7
RyanDBarnett <conner2468@gmail.com>
2020-12-04 11:41:38 -0700

    Add get /port/new AND post /ports routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/ports', to: 'ports#index'
+  get '/ports/new', to: 'ports#new'
   get '/ports/:id', to: 'ports#show'
+  post '/ports', to: 'ports#create'
   get '/ships', to: 'ships#index'
   get '/ships/:id', to: 'ships#show'
   get '/vehicles', to: 'vehicles#index'
