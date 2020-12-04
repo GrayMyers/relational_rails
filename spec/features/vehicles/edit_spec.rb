3d745afa7ab2e5ee496207d194528ef1912af873
Grayson Myers <graymyers10@gmail.com>
2020-12-04 12:02:27 -0700

    Fix test attempting to visit nil vehicle page

--- spec/features/vehicles/edit_spec.rb
+++ spec/features/vehicles/edit_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 describe "I  see a form to edit the parent's attributes:" do
   describe "When I fill out the form with updated information" do
     it "updates parent info and redirects" do
-      plane = Vehicle.new( name:"Boeing 747", passenger_capacity: 100, locked:"false")
-
+      plane = Vehicle.create( name:"Boeing 747", passenger_capacity: 100, locked:"false")
+      
       visit "/vehicles/#{plane.id}/edit"
       expect(page).to have_content("Update Boeing 747")
 
       fill_in(:name, with: "Boeing 757")
       check "locked"
       fill_in(:passenger_capacity, with: 200)
-      click_button "Create"
+      click_button "Update"
 
       expect(current_path).to eq("/vehicles/#{plane.id}")
       expect(page).to have_content("Boeing 757")
