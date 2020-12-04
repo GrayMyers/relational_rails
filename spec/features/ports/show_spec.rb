0066cabec26c3ce4ac95bcf2f1813d19a078600b
RyanDBarnett <conner2468@gmail.com>
2020-12-03 17:35:08 -0700

    Update port show view test to use locally created ports instead of seed file

--- spec/features/ports/show_spec.rb
+++ spec/features/ports/show_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 describe 'As a visitor when I visit a port show page' do
   it "Then I see the port with that id including the port's attributes" do
-    visit '/ports/1'
+    la = Port.create(name: 'Los Angeles', panamax: true, dock_count: 5)
+    ny = Port.create(name: 'New York', panamax: false, dock_count: 3)
+    orleans = Port.create(name: 'New Orleans', panamax: true, dock_count: 2)
 
-    expect(page).to have_content("Los Angeles")
-    expect(page).to have_content("Panamax Capable: true")
-    expect(page).to have_content("Dock Count: 5")
+    visit "/ports/#{la.id}"
 
-    visit '/ports/2'
+    expect(page).to have_content(la.name)
+    expect(page).to have_content("Panamax Capable: #{la.panamax}")
+    expect(page).to have_content("Dock Count: #{la.dock_count}")
 
-    expect(page).to have_content("New York")
-    expect(page).to have_content("Panamax Capable: false")
-    expect(page).to have_content("Dock Count: 3")
+    visit "/ports/#{ny.id}"
+
+    expect(page).to have_content(ny.name)
+    expect(page).to have_content("Panamax Capable: #{ny.panamax}")
+    expect(page).to have_content("Dock Count: #{ny.dock_count}")
   end
 end
