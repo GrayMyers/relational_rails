bf1502678a738c8ae77731bb3e4b57e0d5336f23
Grayson Myers <graymyers10@gmail.com>
2020-12-04 11:24:58 -0700

    Add test for update link on vehicle show page

--- spec/features/vehicles/show_spec.rb
+++ spec/features/vehicles/show_spec.rb
@@ -8,4 +8,13 @@ describe 'as a visitor when I visit the page of a vehicle by id' do
     expect(page).to have_content("Locked: true")
     expect(page).to have_content("Seats: 100")
   end
+
+  it 'Then I see a link to update the parent "Update Parent"' do
+    plane = Vehicle.create(name:"Boeing 747", locked: true, passenger_capacity: 100)
+    visit "vehicles/#{plane[:id]}"
+    expect(page).to have_link("Update Vehicle")
+    click_link "Update Vehicle"
+    expect(current_path).to eq("/vehicles/#{plane[:id]}/edit")
+    expect(page).to have_content("Update")
+  end
 end
