b1b45102a8309aae6a43732e978d0782f827bf4b
Grayson Myers <graymyers10@gmail.com>
2020-12-05 18:38:58 -0700

    Fix typo

--- spec/features/passengers/edit_spec.rb
+++ spec/features/passengers/edit_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 describe "As a visitor when I visit a Child Show page" do
     it "I see a link to update that Child 'Update Child'" do
     plane = Vehicle.create( name:"Boeing 747", passenger_capacity: 100, locked:"false")
-    passenger = plane.Passenger.create(name:"pass 1",driver:false,age:7)
+    passenger = plane.passengers.create(name:"pass 1",driver:false,age:7)
     visit "/passengers/#{passenger.id}/edit"
     expect(page).to have_content("Update pass 1")
 
     fill_in(:name, with: "slightly older pass 1")
-    fill_in(:passenger_capacity, with: 8)
+    fill_in(:age, with: 8)
     click_button "Update"
 
     expect(current_path).to eq("/passengers/#{passenger.id}")
