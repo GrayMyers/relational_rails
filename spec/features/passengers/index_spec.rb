580db2b68e634b23489f2ac1cd748dbbe93f6b48
Grayson Myers <graymyers10@gmail.com>
2020-12-05 17:13:20 -0700

    Add test for index page of passenger table

--- spec/features/passengers/index_spec.rb
+++ spec/features/passengers/index_spec.rb
@@ -1,10 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe 'as a visitor when I visit the vehicle index page' do
   it 'Then I see the name of each vehicle record in the system' do
+    plane = Vehicle.create(name:"Boeing 747", locked: true, passenger_capacity: 100)
+    passenger1 = plane.passengers.create(name: "passenger 1", driver:false, age:37)
+    passenger3 = plane.passengers.create(name: "passenger 3", driver:true, age:65)
     visit '/passengers'
-    expect(page).to have_content("Bill Fox")
-    expect(page).to have_content("Jenna Thompson")
-    expect(page).to have_content("Jane Fox")
+    expect(page).to have_content("passenger 1")
+    expect(page).to have_content("false")
+    expect(page).to have_content("37")
+
+    expect(page).to have_content("passenger 2")
+    expect(page).to have_content("false")
+    expect(page).to have_content("37")
   end
 end
