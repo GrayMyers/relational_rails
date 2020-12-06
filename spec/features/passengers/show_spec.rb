c6f7af330957b6a6549f2bf7a375935cc5f370b7
Grayson Myers <graymyers10@gmail.com>
2020-12-05 17:53:15 -0700

    Add test for show page of passenger

---
+++ spec/features/passengers/show_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "As a visitor When I visit '/child_table_name/:id'" do
+  it "Then I see the child with that id including the child's attributes" do
+    plane = Vehicle.create(name:"Boeing 747", locked: true, passenger_capacity: 100)
+    passenger1 = plane.passengers.create(name: "passenger 1", driver:false, age:37)
+
+    visit "/passengers/#{passenger1.id}"
+    expect(page).to have_content("passenger 1")
+    expect(page).to have_content("Driver: false")
+    expect(page).to have_content("Age: 37")
+  end
+end
