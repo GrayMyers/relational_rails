5feb36514c185d984afc98dc6299a90fa5f87f8e
Grayson Myers <graymyers10@gmail.com>
2020-12-05 17:14:22 -0700

    Clean up old routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   get '/ports/:id', to: 'ports#show'
   get '/ships', to: 'ships#index'
   get '/ships/:id', to: 'ships#show'
+
   post '/vehicles', to: 'vehicles#create'
   get '/vehicles', to: 'vehicles#index'
   get '/vehicles/new', to: 'vehicles#new'
   get 'vehicles/:id', to: 'vehicles#show'
-  post '/vehicles/:id', to: 'vehicles#update'
+  patch '/vehicles/:id', to: 'vehicles#update'
   delete '/vehicles/:id', to: 'vehicles#delete'
   get '/vehicles/:id/edit', to: 'vehicles#edit'
+  
   get '/passengers', to: 'passengers#index'
-  get 'passengers/:id', to: 'passengers#show'
 end
