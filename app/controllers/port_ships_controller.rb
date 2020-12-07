8f8b04a83019544a591c37702963a42117f1ebc7
RyanDBarnett <conner2468@gmail.com>
2020-12-06 17:08:04 -0700

    Add port ships controller create action, ship strong params, and load port before action

--- app/controllers/port_ships_controller.rb
+++ app/controllers/port_ships_controller.rb
@@ -1,9 +1,25 @@
 class PortShipsController < ApplicationController
+  before_action :load_port
   def index
-    @port = Port.find(params[:port_id])
+
   end
 
   def new
 
+  end
+
+  def create
+    @port.ships.create(ship_params)
+    redirect_to "/ports/#{@port.id}/ships"
+  end
+
+  private
+
+  def load_port
+    @port = Port.find(params[:port_id])
+  end
+
+  def ship_params
+    params.permit(:name, :crew_count, :floating)
   end
 end
