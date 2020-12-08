ef9b06b89ac15d2f1eca0be5580793033feaa754
RyanDBarnett <conner2468@gmail.com>
2020-12-07 22:40:28 -0700

    Add ports index recent sorted test

--- spec/features/ports/index_spec.rb
+++ spec/features/ports/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'as a visitor when I visit the port index page' do
+describe 'as a visitor when I visit the ports index page' do
   it 'Then I see the name of each port record in the system' do
     la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5)
     ny = Port.create!(name: 'New York', panamax: false, dock_count: 3)
@@ -11,5 +11,16 @@ describe 'as a visitor when I visit the port index page' do
     expect(page).to have_content(la.name)
     expect(page).to have_content(ny.name)
     expect(page).to have_content(orleans.name)
+  end
+
+  it 'I see the most recently created records in order by recency from top to bottom' do
+    la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5, created_at: 'Mon, 07 Dec 2020 01:00:00 UTC +00:00')
+    orleans = Port.create!(name: 'New Orleans', panamax: true, dock_count: 2, created_at: 'Tues, 08 Dec 2020 01:00:00 UTC +00:00')
+    ny = Port.create!(name: 'New York', panamax: false, dock_count: 3, created_at: 'Wed, 09 Dec 2020 01:00:00 UTC +00:00')
+
+    visit '/ports'
+
+    expect(ny.name).to appear_before(orleans.name)
+    expect(orleans.name).to appear_before(la.name)
   end
 end
