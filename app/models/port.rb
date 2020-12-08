aab7bf96cdb3da3b1ad7e7a919686555706cf4c3
RyanDBarnett <conner2468@gmail.com>
2020-12-07 23:59:24 -0700

    Inherit sorted recent at top from application record

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,7 +1,3 @@
 class Port < ApplicationRecord
   has_many :ships
-
-  def self.sorted_recent_at_top
-    return order(created_at: :desc)
-  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,7 +1,3 @@
 class Ship < ApplicationRecord
   belongs_to :port
-
-  def self.sorted_recent_at_top
-    return order(created_at: :desc)
-  end
 end
