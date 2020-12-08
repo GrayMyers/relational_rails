c73d42d1c299c579bda7d39323a540d4517f4c77
RyanDBarnett <conner2468@gmail.com>
2020-12-07 22:15:03 -0700

    Add ports sorted recent at top helper method test in port model spec

--- spec/models/port_spec.rb
+++ spec/models/port_spec.rb
@@ -4,4 +4,14 @@ describe Port do
   describe 'relationships' do
     it { should have_many :ships }
   end
+
+  describe 'class helper methods' do
+    it 'ports_sorted_recent_at_top' do
+      la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5, created_at: 'Mon, 07 Dec 2020 01:00:00 UTC +00:00')
+      orleans = Port.create!(name: 'New Orleans', panamax: true, dock_count: 2, created_at: 'Tues, 08 Dec 2020 01:00:00 UTC +00:00')
+      ny = Port.create!(name: 'New York', panamax: false, dock_count: 3, created_at: 'Wed, 09 Dec 2020 01:00:00 UTC +00:00')
+
+      expect(Port.ports_sorted_recent_at_top).to eq([la, ny, orleans])
+    end
+  end
 end
