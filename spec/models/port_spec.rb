46724b368f138f98ee0f525c924dddfec65e952a
RyanDBarnett <conner2468@gmail.com>
2020-12-08 13:02:02 -0700

    Add port model ship count helper method test

--- spec/models/port_spec.rb
+++ spec/models/port_spec.rb
@@ -4,4 +4,20 @@ describe Port do
   describe 'relationships' do
     it { should have_many :ships }
   end
+
+  describe 'helper methods' do
+    describe 'ship_count' do
+      it 'should return the number of ships that belong to this port' do
+        la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5)
+        la.ships.create!(name: 'Battleship', floating: true, crew_count: 1500)
+        la.ships.create!(name: 'Destroyer', floating: true, crew_count: 100)
+
+        expect(la.ship_count).to eq(2)
+
+        la.ships.create!(name: 'New Ship', floating: true, crew_count: 100)
+
+        expect(la.ship_count).to eq(3)
+      end
+    end
+  end
 end
