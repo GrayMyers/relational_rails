ed4f2b89e381d8a340b8f83e65080ecfa290d28a
RyanDBarnett <conner2468@gmail.com>
2020-12-07 23:46:42 -0700

    Add ship model and index spec tests

--- spec/features/ships/index_spec.rb
+++ spec/features/ships/index_spec.rb
@@ -2,21 +2,38 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'when I visit /ships' do
-    it "Then I see each ship in the system including the ship's attributes" do
-      la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5)
-      wreck = la.ships.create!(name: 'Shipwreck', floating: false, crew_count: 0)
-      battleship = la.ships.create!(name: 'Battleship', floating: true, crew_count: 1500)
-      destroyer = la.ships.create!(name: 'Destroyer', floating: true, crew_count: 100)
+    before :each do
+      @la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5, created_at: 'Mon, 07 Dec 2020 01:00:00 UTC +00:00')
+      @wreck = @la.ships.create!(name: 'Shipwreck', floating: false, crew_count: 0, created_at: 'Tues, 08 Dec 2020 01:00:00 UTC +00:00')
+      @battleship = @la.ships.create!(name: 'Battleship', floating: true, crew_count: 1500, created_at: 'Thurs, 10 Dec 2020 01:00:00 UTC +00:00')
+      @destroyer = @la.ships.create!(name: 'Destroyer', floating: true, crew_count: 100, created_at: 'Weds, 9 Dec 2020 01:00:00 UTC +00:00')
 
       visit '/ships'
-      
-      expect(page).to have_content("Shipwreck")
-      expect(page).to have_content("Floating: false")
-      expect(page).to have_content("Crew Count: 0")
+    end
 
-      expect(page).to have_content("Battleship")
-      expect(page).to have_content("Floating: true")
-      expect(page).to have_content("Crew Count: 1500")
+    it "Then I see each ship in the system including the ship's attributes" do
+      expect(page).to have_content(@wreck.name)
+      expect(page).to have_content("Floating: #{@wreck.floating}")
+      expect(page).to have_content("Crew Count: #{@wreck.crew_count}")
+
+      expect(page).to have_content(@battleship.name)
+      expect(page).to have_content("Floating: #{@battleship.floating}")
+      expect(page).to have_content("Crew Count: #{@battleship.crew_count}")
+    end
+
+    it 'I see the most recently created records in order by recency from top to bottom' do
+      expect(@battleship.name).to appear_before(@destroyer.name)
+      expect(@destroyer.name).to appear_before(@wreck.name)
+    end
+
+    it 'I see the DateTime(s) next to each of the records in a reasonably formatted manner' do
+      within(".ship-#{@battleship.id}") do
+        expect(page).to have_content("Created on 12/10/2020")
+      end
+
+      within(".ship-#{@wreck.id}") do
+        expect(page).to have_content("Created on 12/08/2020")
+      end
     end
   end
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -4,4 +4,15 @@ describe Ship do
   describe 'relationships' do
     it { should belong_to :port }
   end
+
+  describe 'class helper methods' do
+    it 'sorted_recent_at_top' do
+      @la = Port.create!(name: 'Los Angeles', panamax: true, dock_count: 5, created_at: 'Mon, 07 Dec 2020 01:00:00 UTC +00:00')
+      @wreck = @la.ships.create!(name: 'Shipwreck', floating: false, crew_count: 0, created_at: 'Tues, 08 Dec 2020 01:00:00 UTC +00:00')
+      @battleship = @la.ships.create!(name: 'Battleship', floating: true, crew_count: 1500, created_at: 'Thurs, 10 Dec 2020 01:00:00 UTC +00:00')
+      @destroyer = @la.ships.create!(name: 'Destroyer', floating: true, crew_count: 100, created_at: 'Weds, 9 Dec 2020 01:00:00 UTC +00:00')
+
+      expect(@la.ships.sorted_recent_at_top).to eq([@battleship, @destroyer, @wreck])
+    end
+  end
 end
