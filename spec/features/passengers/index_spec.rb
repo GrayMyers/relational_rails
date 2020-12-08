419f7e1e5d39a7791255374601b3b7e704f41e23
Grayson Myers <graymyers10@gmail.com>
2020-12-07 17:38:54 -0700

    Add test for element ordering by recency for passengers index

--- spec/features/passengers/index_spec.rb
+++ spec/features/passengers/index_spec.rb
@@ -14,4 +14,18 @@ describe 'as a visitor when I visit the passenger index page' do
     expect(page).to have_content("Driver: true")
     expect(page).to have_content("Age: 65")
   end
+
+  it "Orders records by recency" do
+    plane = Vehicle.create(name:"Boeing 747", locked: true, passenger_capacity: 100)
+    passenger1 = plane.passengers.create(name: "passenger 1", driver:false, age:37)
+    passenger2 = plane.passengers.create(name: "passenger 2", driver:true, age:65)
+    passenger3 = plane.passengers.create(name: "passenger 3", driver:false, age:5)
+
+    visit '/passengers'
+
+    #check order of elements as they appear on the page
+    #page.body.should =~ /Boeing 747.*Toyota Highlander.*Ford Raptor/
+    expect(page.body.index("passenger 3")).to be < page.body.index("passenger 2")
+    expect(page.body.index("passenger 2")).to be < page.body.index("passenger 1")
+  end
 end
