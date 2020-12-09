8ea71480b0d4121e13a4601d881a77656ddee77b
RyanDBarnett <conner2468@gmail.com>
2020-12-08 21:11:57 -0700

    Add port index spec dock count great than input test

--- spec/features/ports/index_spec.rb
+++ spec/features/ports/index_spec.rb
@@ -28,4 +28,18 @@ describe 'as a visitor when I visit the ports index page' do
   it 'I see the records that have panamax `true` above/before the records that have a false' do
     expect(@la.name).to appear_before(@ny.name)
   end
+
+  describe 'I see a form that allows me to input a number value' do
+    describe 'When I input a number value and click the submit button' do
+      it 'The ports index page renders only ports with dock_counts greater than the input number value' do
+        fill_in 'dock_count_limit', with: '2'
+
+        click_on 'Only return ports with more docks than the given `Dock Count Threshold Number`'
+
+        expect(page).to have_content(@la.name)
+        expect(page).to have_content(@ny.name)
+        expect(page).to_not have_content(@orleans.name)
+      end
+    end
+  end
 end
