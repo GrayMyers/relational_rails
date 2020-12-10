98d0175a825401f7a005a6b983cd1fad34a3282d
Grayson Myers <graymyers10@gmail.com>
2020-12-09 20:47:08 -0700

    Add links for each parent

--- spec/features/vehicles/index_spec.rb
+++ spec/features/vehicles/index_spec.rb
@@ -90,5 +90,16 @@ describe 'as a visitor when I visit the vehicle index page' do
     expect(page).to have_no_content(car.name)
   end
 
+  it "Has a link for each parent that redirects to parent show page" do
+    car = Vehicle.create(name:"Toyota Highlander", locked: false, passenger_capacity: 4)
+
+    visit '/vehicles'
+
+    expect(page).to have_link(car.name)
+    click_on car.name
+
+    expect(current_path).to eq("/vehicles/#{car.id}")
+  end
+
 
 end
